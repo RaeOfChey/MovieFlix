e34bc9381b92b6c394a51b5781ae6916b9cd631d
RaeofChey <cheyennaraelynn@gmail.com>
2024-10-22 08:56:01 -0500

    Added sql and ts files

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO movies (movie_name)
+VALUES ('Inglourious Basterds'),
+       ('Alien'),
+       ('Lord of the Rings'),
+       ('Shawshank Redemption'),
+       ('Forest Gump');
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, 'Explosive, brutal, and brilliantly acted—pure Tarantino madness at its finest.'),
+       (2, 'Claustrophobic, intense, and endlessly rewatchable—a genre-defining masterpiece.'),
+       (1, 'Tarantino delivers a bold, thrilling, and unpredictable war film with unforgettable characters.'),
+       (3, 'A perfect blend of fantasy, adventure, and heart—Tolkien’s vision fully realized.'),
+       (5, 'Charming, emotional, and iconic, Forrest’s journey is pure cinematic magic.'),
+       (1, 'A wild revenge fantasy with sharp dialogue and incredible tension.'),
+       (5, 'Tom Hanks shines i nthis heartwarming tale of life, love, and destiny.');
+       
